8fff8b77b56a59cfc14aa07e84818567d1891442
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-09-08 10:14:12 +0530

    minor changes 2

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
     origin:"https://nirjus-portfolio.vercel.app",
-    optionsSuccessStatus:200,
+    credentials:true,
 }))
 
 

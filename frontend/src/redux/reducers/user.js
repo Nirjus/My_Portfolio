8fe514838d635aabbbf84da1c97cd59fc8377961
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-09-15 02:33:07 +0530

    add isAdmin in load user

--- frontend/src/redux/reducers/user.js
+++ frontend/src/redux/reducers/user.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export const userReducer = createReducer(initialState, {
     GET_USER_REQUEST: (state) => {
-        state.loading = true;
+        state.loading = false;
     },
     GET_USER_SUCCESS: (state, action) => {
         state.loading = false;
@@ -44,15 +44,16 @@ export const loginReducer = createReducer(
 
     LOAD_USER_REQUEST: (state) => {
         state.loading = true;
+        state.isAdmin = false;
     },
     LOAD_USER_SUCCESS: (state, action) => {
         state.loading = false;
-       
+        state.isAdmin = true;
         state.user = action.payload;
     },
     LOAD_USER_FAILURE: (state, action) => {
         state.loading = false;
-       
+        state.isAdmin = false;
         state.error = action.payload;
     },
 
